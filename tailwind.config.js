2495ada5381ea901cbb682ed4e4ba7221902cd61
cihan base <cihanbas07@gmail.com>
2024-06-24 02:12:39 +0300

    tailwind ayarlari yapildi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      gray: "rgb(199, 199, 204)",
+      white: "#ffffff",
+      "bg-2": "#959595",
+      primary: "#D0810A",
+      error: "#FF2323",
+    },
     extend: {},
   },
   plugins: [],
