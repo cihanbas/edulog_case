fd704e5dc44d536c48eee6086ebb92865a832f87
cihan base <cihanbas07@gmail.com>
2024-06-24 02:30:48 +0300

    tema navigasyona eklendi

--- src/navigation/main.tsx
+++ src/navigation/main.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
+import { COLORS } from "src/utils/theme";
 import { TabNavigator } from "./tabs";
 import { RootStackParamList } from "./types";
 
@@ -8,7 +9,7 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const MainStack = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={{ dark: false, colors: COLORS }}>
       <Stack.Navigator>
         <Stack.Screen
           name="Tabs"
