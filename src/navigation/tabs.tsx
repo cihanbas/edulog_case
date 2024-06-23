a356657e6ad8628b878056c0ed82d984a8c2132a
cihan base <cihanbas07@gmail.com>
2024-06-24 02:12:59 +0300

    TabStack'deki ekranlarin ikonlari eklendi

--- src/navigation/tabs.tsx
+++ src/navigation/tabs.tsx
@@ -5,16 +5,48 @@ import HomeScreen from "../screens/Home/HomeScreen";
 import { TabParamList } from "./types";
 import WishList from "../screens/WishList/WishListScreen";
 import AuthScreen from "../screens/Account/Auth/AuthScreen";
-
+import { AntDesign } from "@expo/vector-icons";
 const Tab = createBottomTabNavigator<TabParamList>();
 
 export const TabNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Cart" component={CartScreen} />
-      <Tab.Screen name="WishList" component={WishList} />
-      <Tab.Screen name="Account" component={AuthScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="shoppingcart" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="WishList"
+        component={WishList}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="hearto" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={AuthScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="user" size={24} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
