a2fc860bd80becc2b2d0bd7ae515df11de98b2bf
cihan base <cihanbas07@gmail.com>
2024-06-24 02:02:07 +0300

    Fontlar degistirildi

--- assets/fonts/fontFamilyTypes.ts
+++ assets/fonts/fontFamilyTypes.ts
@@ -1,7 +1,14 @@
 export enum FontFamily {
-  light = "AvenirNextCyr-Light",
-  Regular = "AvenirNextCyr-Regular",
-  Medium = "AvenirNextCyr-Medium",
-  Demi = "AvenirNextCyr-Demi",
-  Bold = "AvenirNextCyr-Bold",
+  light = "AvenirLTProBook",
+  Regular = "AvenirLTProRoman",
+  Medium = "AvenirLTProMedium",
+  Semi = "AvenirLTProHeavy",
+  Bold = "AvenirLTProBlack",
 }
+export const fonts = {
+  AvenirLTProBook: require("./AvenirLTProBook.otf"),
+  AvenirLTProRoman: require("./AvenirLTProRoman.otf"),
+  AvenirLTProMedium: require("./AvenirLTProMedium.otf"),
+  AvenirLTProHeavy: require("./AvenirLTProHeavy.otf"),
+  AvenirLTProBlack: require("./AvenirLTProBlack.otf"),
+};
