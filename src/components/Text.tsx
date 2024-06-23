235a383aa998563ad99b9b8fe3e5baa350f1b025
cihan base <cihanbas07@gmail.com>
2024-06-24 02:29:43 +0300

    Text Komponenti olusturuldu

---
+++ src/components/Text.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+import { StyleSheet, Text, TextProps } from "react-native";
+import { FontFamily } from "../../assets/fonts/fontFamilyTypes";
+import { normalize } from "../utils/normalize";
+interface Props extends TextProps {
+  fontFamily?: FontFamily;
+  color?: string;
+  fontSize?: number;
+}
+const NativeText: FC<Props> = (props) => {
+  return (
+    <Text
+      {...props}
+      style={StyleSheet.flatten([
+        {
+          fontFamily: props.fontFamily || FontFamily.Medium,
+          color: props.color || "black",
+          fontSize: props.fontSize || normalize(14),
+        },
+        props.style,
+      ])}
+    >
+      {props.children}
+    </Text>
+  );
+};
+
+export default NativeText;
