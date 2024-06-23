0e97b99c2aca56fb7f1749e60b356a05d079966b
cihan base <cihanbas07@gmail.com>
2024-06-24 01:28:38 +0300

    yeni fontlar eklendi

---
+++ assets/fonts/fontFamilyTypes.ts
@@ -0,0 +1,7 @@
+export enum FontFamily {
+  light = "AvenirNextCyr-Light",
+  Regular = "AvenirNextCyr-Regular",
+  Medium = "AvenirNextCyr-Medium",
+  Demi = "AvenirNextCyr-Demi",
+  Bold = "AvenirNextCyr-Bold",
+}
