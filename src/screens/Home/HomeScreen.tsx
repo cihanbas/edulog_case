85e9fa1542c174cd77cfed17d0377778349b16b7
cihan base <cihanbas07@gmail.com>
2024-06-25 17:20:15 +0300

    home page is completed

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,48 +1,118 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { FC, useState } from "react";
+import {
+  FlatList,
+  ListRenderItem,
+  Pressable,
+  StyleSheet,
+  View,
+} from "react-native";
 
+import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+import { useQuery } from "@tanstack/react-query";
+import { FontFamily, fonts } from "assets/fonts/fontFamilyTypes";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { FontFamily } from "../../../assets/fonts/fontFamilyTypes";
-import NativeText from "../../components/Text";
-const HomeScreen = () => {
-  const [fontsLoaded, fontError] = useFonts({
-    "AvenirNextCyr-Light": require("../../../assets/fonts/AvenirNextCyr-Light.ttf"),
-    "AvenirNextCyr-Regular": require("../../../assets/fonts/AvenirNextCyr-Regular.ttf"),
-    "AvenirNextCyr-Medium": require("../../../assets/fonts/AvenirNextCyr-Medium.ttf"),
-    "AvenirNextCyr-Demi": require("../../../assets/fonts/AvenirNextCyr-Demi.ttf"),
-    "AvenirNext-Bold": require("../../../assets/fonts/AvenirNextCyr-Bold.ttf"),
+import { SafeAreaView } from "react-native-safe-area-context";
+import { NativeText } from "src/components";
+import SearchBar from "src/components/SearchBar";
+import { TabParamList } from "src/navigation/types";
+import { api } from "src/services/api";
+import { AllCategoryName } from "src/types/Categories";
+import { COLORS } from "src/utils/theme";
+import ProductList from "./ProductList";
+type HomeScreenProps = BottomTabScreenProps<TabParamList, "Home">;
+let renderCount = 0;
+const HomeScreen: FC<HomeScreenProps> = ({ navigation }) => {
+  const [selectCategory, setSelectCategory] = useState("");
+  const { data, isLoading } = useQuery({
+    queryKey: ["categories"],
+    queryFn: async () => {
+      const response = await (await api.getAllCategories()).data;
+      console.log("response", response.length);
+      if (response.length > 0) {
+        return [AllCategoryName.All, ...response];
+      }
+      return [];
+    },
   });
-
+  const [fontsLoaded, fontError] = useFonts(fonts);
   const onLayoutRootView = React.useCallback(async () => {
     if (fontsLoaded || fontError) {
       await SplashScreen.hideAsync();
     }
   }, [fontsLoaded, fontError]);
-
   if (!fontsLoaded && !fontError) {
     return null;
   }
+  const navigateToSearch = () => {
+    navigation.navigate("Search");
+  };
+  const updateCategory = (categoryName: string) => {
+    if (categoryName === AllCategoryName.All) {
+      setSelectCategory("");
+    } else {
+      setSelectCategory(categoryName);
+    }
+  };
+  const getCategoryColor = (item: string) => {
+    if (selectCategory === "" && item === AllCategoryName.All) {
+      return COLORS.primary;
+    }
+    return selectCategory === item ? COLORS.primary : COLORS.black;
+  };
+  renderCount += 1;
+  console.log("renderCount", renderCount);
+  const renderCategories: ListRenderItem<string> = ({ item, index }) => {
+    return (
+      <Pressable style={styles.category} onPress={() => updateCategory(item)}>
+        <NativeText
+          fontFamily={FontFamily.Medium}
+          style={[styles.categoryText, { color: getCategoryColor(item) }]}
+        >
+          {item}
+        </NativeText>
+      </Pressable>
+    );
+  };
   return (
-    <View onLayout={onLayoutRootView}>
-      <Text>HomeScreen</Text>
-      <NativeText style={{ fontFamily: FontFamily.light }}>
-        Avenir LT Pro 35 Light
-      </NativeText>
-      <NativeText>Avenir LT Pro 35 Light</NativeText>
-      <NativeText style={{ fontFamily: "AvenirNextCyr-Medium" }}>
-        Avenir LT Pro 35 Light
-      </NativeText>
-      <NativeText style={{ fontFamily: "AvenirNextCyr-Demi" }}>
-        Avenir LT Pro 35 Light
-      </NativeText>
-      <NativeText style={{ fontFamily: FontFamily.Bold }}>
-        Avenir LT Pro 35 Light
-      </NativeText>
-    </View>
+    <SafeAreaView
+      onLayout={onLayoutRootView}
+      className="flex-1"
+      edges={["top"]}
+    >
+      <Pressable onPress={navigateToSearch}>
+        <View pointerEvents="none">
+          <SearchBar editable={false} />
+        </View>
+      </Pressable>
+      <View>
+        <FlatList
+          data={data || []}
+          renderItem={renderCategories}
+          keyExtractor={(item) => item}
+          horizontal
+          style={styles.categories}
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
+
+      <ProductList categoryName={selectCategory} />
+    </SafeAreaView>
   );
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  categories: {
+    alignSelf: "flex-start",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    margin: 10,
+  },
+  category: {
+    padding: 8,
+    alignSelf: "flex-start",
+  },
+  categoryText: {
+    textTransform: "uppercase",
+  },
+});
---
+++ src/screens/Home/ProductList.tsx
@@ -0,0 +1,103 @@
+import { AntDesign } from "@expo/vector-icons";
+import { useQuery } from "@tanstack/react-query";
+import { FontFamily } from "assets/fonts/fontFamilyTypes";
+import React, { FC, memo } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  ImageBackground,
+  ListRenderItem,
+  Pressable,
+  StyleSheet,
+  View,
+} from "react-native";
+import { NativeText } from "src/components";
+import { api } from "src/services/api";
+import { addItem } from "src/store/features/cart/cartSlice";
+import { useAppDispatch } from "src/store/hooks";
+import { Product } from "src/types/Product";
+import { width } from "src/utils/constants";
+import { COLORS } from "src/utils/theme";
+interface Props {
+  categoryName: string;
+}
+const ProductList: FC<Props> = ({ categoryName }) => {
+  const { data } = useQuery({
+    queryKey: ["allProduct", categoryName],
+    queryFn: async () => await (await api.getAllProducts(categoryName)).data,
+  });
+  const dispatch = useAppDispatch();
+  const addToBasket = (item: Product) => {
+    dispatch(addItem(item));
+  };
+  const renderProduct: ListRenderItem<Product> = ({ item }) => {
+    return (
+      <View style={styles.item}>
+        <ImageBackground
+          source={{ uri: item.image }}
+          style={styles.image}
+          resizeMode="contain"
+        >
+          <Pressable style={styles.favContainer}>
+            <AntDesign name="hearto" size={24} color={COLORS.notification} />
+          </Pressable>
+        </ImageBackground>
+        <View className="my-2">
+          <NativeText className="my-2" numberOfLines={2}>
+            {item.title}
+          </NativeText>
+          <NativeText
+            fontFamily={FontFamily.Bold}
+            className="color-primary text-right"
+          >
+            ${item.price}
+          </NativeText>
+          <Pressable style={styles.btn} onPress={() => addToBasket(item)}>
+            <NativeText style={styles.btnText}>Sepete Ekle</NativeText>
+          </Pressable>
+        </View>
+      </View>
+    );
+  };
+  return (
+    <FlatList
+      contentContainerStyle={{ flexGrow: 1, paddingTop: 16 }}
+      data={data || []}
+      renderItem={renderProduct}
+      numColumns={2}
+      ListEmptyComponent={<ActivityIndicator />}
+    />
+  );
+};
+
+export default memo(ProductList);
+const contentWidth = width / 2 - 20;
+const styles = StyleSheet.create({
+  image: {
+    height: contentWidth,
+    width: contentWidth,
+    alignItems: "flex-end",
+  },
+  item: {
+    width: contentWidth,
+    marginLeft: 10,
+    marginBottom: 20,
+  },
+  favContainer: {
+    margin: 8,
+    borderRadius: 50,
+    padding: 4,
+    backgroundColor: "white",
+  },
+  btn: {
+    padding: 10,
+    backgroundColor: COLORS.primary,
+    borderRadius: 9,
+    marginTop: 8,
+  },
+  btnText: {
+    color: "white",
+    fontFamily: FontFamily.Bold,
+    textAlign: "center",
+  },
+});
