f77c6a75123c27e1cbb9afc9bc9e6f31d807e71f
cihan base <cihanbas07@gmail.com>
2024-07-31 14:04:37 +0300

    merge

---
+++ App2.tsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MainStack } from "./src/navigation/main";
+import { Provider } from "react-redux";
+import { persistor, store } from "src/store/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <MainStack />
+        </PersistGate>
+      </Provider>
+    </QueryClientProvider>
+  );
+}
---
+++ firebaseConfig.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAigNuPaBtLI2BSTRzpM48ROGCHUj08VYs",
+  authDomain: "ders14firebasecihan.firebaseapp.com",
+  projectId: "ders14firebasecihan",
+  storageBucket: "ders14firebasecihan.appspot.com",
+  messagingSenderId: "538564218878",
+  appId: "1:538564218878:web:39c28adfde0ac1312efaaf",
+};
+
+// Initialize Firebase
